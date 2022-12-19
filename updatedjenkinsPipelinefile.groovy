17f96f261393b57618f5b95f5fd8a8ac5134b073
khus-al <115480191+khus-al@users.noreply.github.com>
2022-12-19 10:39:41 +0530

    Update updatedjenkinsPipelinefile.groovy

--- updatedjenkinsPipelinefile.groovy
+++ updatedjenkinsPipelinefile.groovy
@@ -4,19 +4,19 @@ parameters {
 string(name: 'myParameter', defaultValue: 'myVal', description: 'Enter Parameter value?')
 }
 stages {
-stage('Build') {
+stage('Build1') {
 steps {
 echo 'Building..'
 echo "Running ${env.BUILD_ID} on ${env.JENKINS_URL}"
 }
 }
-stage('Test') {
+stage('Test1') {
 steps {
 echo 'Testing..'
 echo "${params.myParameter} is value retrieved!"
 }
 }
-stage('Deploy') {
+stage('Deploy1') {
 steps {
 echo 'Deploying....'
 }
